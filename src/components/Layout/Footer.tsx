f8d1efd28c6143743a2504cfc405abcefbf49a19
Pablo Pereira <pablopereira27@gmail.com>
2026-05-31 12:07:15 -0300

    chore: remove boilerplate and setup initial layout structure
    - Deleted unnecessary default files and sample data
    - Created base folders for layouts and components
    - Prepared project structure for future routing and features

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>2026 - Dot Digital - EAD</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+function Layout() {
+  return (
+    <div className="appContainer">
+      <Header />
+      <main className="mainContent">
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
